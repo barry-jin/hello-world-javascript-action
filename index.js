51c8140f4822e6fdfa360a42f44dd2eb1308523f
barry-jin <barryjin1995@gmail.com>
2020-08-27 10:39:23 -0700

    This is the second commit

--- index.js
+++ index.js
@@ -94,6 +94,8 @@ async function run() {
 
     // This is a comment1
 
+    // This is a comment2
+
     // python3 -m pdb tools/batch/submit-job.py --region us-east-1 --source-ref numpy --job-type c5n.18x --name test_project --work-dir scripts/conversion_toolkits --remote https://github.com/barry-jin/gluon-nlp/ --command 'python3 hello_world.py' --wait 
 }
 
